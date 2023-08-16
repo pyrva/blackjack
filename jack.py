c49c2627fb5e57034ef1d33637c56df975538d99
Chris May <chris@may.org>
2023-08-16 10:03:16 -0400

    Add changes from 8/15 meeting

--- jack.py
+++ jack.py
@@ -1,16 +1,35 @@
-from deck import Deck
+from dataclasses import dataclass
+
+from deck import Deck, Card
 
 
+def evaluate_hand(hand):
+    values = [card.value for card in hand]
+    if 1 in values:
+        #do something
+        pass
+    return sum(value)
+
+@dataclass
+class Player:
+    hand: list[Card]
+
+    # @property
+    def is_bust(self):
+        return evaluate_hand(self.hand) > 21
 
 class Game(object):
 
-    def __init__(self):
+    def __init__(self, players=1):
         self.cards = Deck(include_jokers=False)
         self.cards.shuffle()
-        self.dealer_hand = self.get_hand()
-        self.player_hand = self.get_hand()
+        self.players = [Player(self.get_hand()) for _ in range(players + 1)]
+
 
     def get_hand(self):
         hand = [self.cards.deal(), self.cards.deal()]
         return hand
 
+
+    def deal(self, hand):
+        hand.append(self.cards.deal())
--- tests/test_jack.py
+++ tests/test_jack.py
@@ -1,4 +1,4 @@
-from jack import Game
+from jack import Game, Player, evaluate_hand
 from deck import Card
 
 def test_hand_dealt():
@@ -16,13 +16,25 @@ def test_cards_dont_match():
 def test_game_start():
     game = Game()
     assert len(game.cards) == 48
-    assert len(game.dealer_hand) == 2
-    assert len(game.player_hand) == 2
+
 
 def test_player_hand_score():
     game = Game()
 
-    hand = [Card(),Card()]
-    assert game.evaluate()
+    hand = [Card(suit='spades', value=2),Card(suit='spades', value=3)]
+    assert evaluate_hand(hand) == 5
 
-# Check count of cards go down over time
+def test_deal_one_card():
+    game = Game()
+    game.deal(game.players[0].hand)
+    game.deal(game.players[1].hand)
+    assert len(game.players[0].hand) == 3
+    assert len(game.players[1].hand) == 3
+
+
+    # Check count of cards go down over time
+
+def test_hand_bust():
+    player = Player([Card(suit='spades', value=10), Card(suit='spades', value=10), Card(suit='spades', value=2)])
+
+    assert player.is_bust() == True
