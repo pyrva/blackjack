7f00c3760da9f6f7b1beb15a31352e91c448e576
Adam Emery <aqemery@gmail.com>
2023-10-18 09:34:13 -0400

    oct 17 updates

--- jack.py
+++ jack.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
 
-from deck import Deck, Card
+from deck import Deck, Card, Suit
 
 
 def evaluate_hand(hand):
     # values = [card.value for card in hand if card.value < 10 else 10]
     values = []
-    #ace count variable
+    # ace count variable
 
     for card in hand:
-        if card.value <=10:
+        if card.value <= 10:
             values.append(card.value)
         else:
             values.append(10)
@@ -20,6 +20,7 @@ def evaluate_hand(hand):
         pass
     return value
 
+
 @dataclass
 class Player:
     hand: list[Card]
@@ -28,18 +29,50 @@ class Player:
     def is_bust(self):
         return evaluate_hand(self.hand) > 21
 
+    # def __str__(self):
+    #     print(self.hand)
+    #     return ''.join([f"{card.value}{card.suit.value} " for card in self.hand])
+
+
 class Game(object):
-
-    def __init__(self, players=1):
-        self.cards = Deck(include_jokers=False)
-        self.cards.shuffle()
+    def __init__(self, players=1, initial_deck=None):
+        """
+        Initial_deck: list of cards with a specific order to deal.
+        """
+        self.cards = initial_deck
+        if not self.cards:
+            self.cards = Deck(include_jokers=False)
+            self.cards.shuffle()
         self.players = [Player(self.get_hand()) for _ in range(players + 1)]
-
 
     def get_hand(self):
         hand = [self.cards.deal(), self.cards.deal()]
         return hand
 
-
     def deal(self, hand):
         hand.append(self.cards.deal())
+
+    def play(self, defined_actions=None):
+        for i, player in enumerate(self.players):
+            while not player.is_bust():
+                print("-" * 20)
+                print("player_id: ", i, "#cards: ", len(player.hand), "cards: ", player)
+                if defined_actions:
+                    print(defined_actions)
+                    action = defined_actions.pop(0)
+                    print(defined_actions)
+                else:
+                    action = input("hit (h) or stand (s)")
+
+                if action == "h":
+                    self.deal(player.hand)
+                    print(player)
+                else:
+                    print(player)
+                    break
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.play(["h", "s", "s"])
+    print(game)
--- tests/test_jack.py
+++ tests/test_jack.py
@@ -1,5 +1,5 @@
 import pytest
-from deck import Card, Suit, Value
+from deck import Card, Suit, Value, Deck
 
 from jack import Game, Player, evaluate_hand
 
@@ -24,7 +24,7 @@ def test_game_start():
 def test_player_hand_score():
     Game()
 
-    hand = [Card(suit='spades', value=2), Card(suit='spades', value=3)]
+    hand = [Card(suit="spades", value=2), Card(suit="spades", value=3)]
     assert evaluate_hand(hand) == 5
 
 
@@ -39,69 +39,164 @@ def test_deal_one_card():
 
 
 def test_hand_bust():
-    player = Player([Card(suit='spades', value=10), Card(suit='spades', value=10), Card(suit='spades', value=2)])
+    player = Player(
+        [
+            Card(suit="spades", value=10),
+            Card(suit="spades", value=10),
+            Card(suit="spades", value=2),
+        ]
+    )
 
     assert player.is_bust() is True
 
 
-@pytest.mark.parametrize('first_card, second_card, expected_score', [
-    (Card(suit=Suit.Spades, value=Value.Two), Card(suit=Suit.Hearts, value=Value.Three), 5),
-    (Card(suit=Suit.Diamonds, value=Value.Six), Card(suit=Suit.Clubs, value=Value.Eight), 14),
-])
-def test_score_is_calculated_correctly_for_number_cards(first_card, second_card, expected_score):
-    '''The score should be the sum of the values of the cards.'''
+@pytest.mark.parametrize(
+    "first_card, second_card, expected_score",
+    [
+        (
+            Card(suit=Suit.Spades, value=Value.Two),
+            Card(suit=Suit.Hearts, value=Value.Three),
+            5,
+        ),
+        (
+            Card(suit=Suit.Diamonds, value=Value.Six),
+            Card(suit=Suit.Clubs, value=Value.Eight),
+            14,
+        ),
+    ],
+)
+def test_score_is_calculated_correctly_for_number_cards(
+    first_card, second_card, expected_score
+):
+    """The score should be the sum of the values of the cards."""
     assert evaluate_hand([first_card, second_card]) == expected_score
 
 
-@pytest.mark.parametrize('first_card, second_card, expected_score', [
-    (Card(suit=Suit.Spades, value=Value.Eight), Card(suit=Suit.Hearts, value=Value.Jack), 18),
-    (Card(suit=Suit.Spades, value=Value.Eight), Card(suit=Suit.Hearts, value=Value.Queen), 18),
-    (Card(suit=Suit.Spades, value=Value.Eight), Card(suit=Suit.Hearts, value=Value.King), 18),
-])
-def test_score_is_calculated_correctly_for_face_cards(first_card, second_card, expected_score):
-    '''The score should be the sum of the values of the cards with face cards having a value of 10.'''
+@pytest.mark.parametrize(
+    "first_card, second_card, expected_score",
+    [
+        (
+            Card(suit=Suit.Spades, value=Value.Eight),
+            Card(suit=Suit.Hearts, value=Value.Jack),
+            18,
+        ),
+        (
+            Card(suit=Suit.Spades, value=Value.Eight),
+            Card(suit=Suit.Hearts, value=Value.Queen),
+            18,
+        ),
+        (
+            Card(suit=Suit.Spades, value=Value.Eight),
+            Card(suit=Suit.Hearts, value=Value.King),
+            18,
+        ),
+    ],
+)
+def test_score_is_calculated_correctly_for_face_cards(
+    first_card, second_card, expected_score
+):
+    """The score should be the sum of the values of the cards with face cards having a value of 10."""
     assert evaluate_hand([first_card, second_card]) == expected_score
 
 
 # @pytest.mark.xfail
-@pytest.mark.parametrize('hand, expected_score', [
-    ([Card(suit=Suit.Spades, value=Value.Ace), Card(suit=Suit.Hearts, value=Value.Ace)], 12),
-    ([Card(suit=Suit.Spades, value=Value.Ace), Card(suit=Suit.Hearts, value=Value.Six),
-      Card(suit=Suit.Hearts, value=Value.Three)], 20),
-    ([Card(suit=Suit.Spades, value=Value.Ace), Card(suit=Suit.Hearts, value=Value.Six),
-      Card(suit=Suit.Hearts, value=Value.King)], 17),
-    ([Card(suit=Suit.Hearts, value=Value.King), Card(suit=Suit.Hearts, value=Value.Ace)], 21)
-])
+@pytest.mark.parametrize(
+    "hand, expected_score",
+    [
+        (
+            [
+                Card(suit=Suit.Spades, value=Value.Ace),
+                Card(suit=Suit.Hearts, value=Value.Ace),
+            ],
+            12,
+        ),
+        (
+            [
+                Card(suit=Suit.Spades, value=Value.Ace),
+                Card(suit=Suit.Hearts, value=Value.Six),
+                Card(suit=Suit.Hearts, value=Value.Three),
+            ],
+            20,
+        ),
+        (
+            [
+                Card(suit=Suit.Spades, value=Value.Ace),
+                Card(suit=Suit.Hearts, value=Value.Six),
+                Card(suit=Suit.Hearts, value=Value.King),
+            ],
+            17,
+        ),
+        (
+            [
+                Card(suit=Suit.Hearts, value=Value.King),
+                Card(suit=Suit.Hearts, value=Value.Ace),
+            ],
+            21,
+        ),
+        (
+            [
+                Card(suit=Suit.Hearts, value=Value.Ace),
+                Card(suit=Suit.Clubs, value=Value.Ace),
+                Card(suit=Suit.Hearts, value=Value.Six),
+            ],
+            18,
+        ),
+    ],
+)
 def test_score_is_calculated_correctly_for_aces(hand, expected_score):
-    '''The score should be the sum of the values of the cards with aces having a value of 1 or 11.'''
+    """The score should be the sum of the values of the cards with aces having a value of 1 or 11."""
     assert evaluate_hand(hand) == expected_score
 
 
+# @pytest.mark.skip
 def test_correct_number_of_cards_on_hit():
-    '''The player should have one more card after hitting.'''
-    player_hand = [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
-    assert len(player_hand) == 2
+    """The player should have one more card after hitting."""
+    starting_deck = Deck(
+        [
+            Card(suit=Suit.Spades, value=Value.Ten),
+            Card(suit=Suit.Spades, value=Value.Eight),
+            Card(suit=Suit.Diamonds, value=Value.Ten),
+            Card(suit=Suit.Diamonds, value=Value.Eight),
+            Card(suit=Suit.Clubs, value=Value.Ten),
+            Card(suit=Suit.Clubs, value=Value.Eight),
+        ]
+    )
     game = Game()
-    game.deal(player_hand)
-    assert len(player_hand) == 3
+
+    assert len(game.players[0].hand) == 2
+    game.play(["h"])
+    assert len(game.players[0].hand) == 3
 
 
 @pytest.mark.xfail
 def test_correct_number_of_cards_on_stand():
-    '''The player should have the same number of cards after standing.'''
-    player_hand = [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
+    """The player should have the same number of cards after standing."""
+    player_hand = [
+        Card(suit=Suit.Spades, value=Value.Ten),
+        Card(suit=Suit.Spades, value=Value.Eight),
+    ]
     assert len(player_hand) == 2
     # Stand
     assert len(player_hand) == 2
 
 
-
-@pytest.mark.parametrize('first_card, second_card, hit', [
-    (Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Hearts, value=Value.Six), True),
-    (Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Hearts, value=Value.Seven), False),
-])
+@pytest.mark.parametrize(
+    "first_card, second_card, hit",
+    [
+        (
+            Card(suit=Suit.Spades, value=Value.Ten),
+            Card(suit=Suit.Hearts, value=Value.Six),
+            True,
+        ),
+        (
+            Card(suit=Suit.Spades, value=Value.Ten),
+            Card(suit=Suit.Hearts, value=Value.Seven),
+            False,
+        ),
+    ],
+)
 def test_dealer_must_hit(first_card, second_card, hit):
-    '''The dealer must hit if their score is less than 17.'''
+    """The dealer must hit if their score is less than 17."""
     hand = [first_card, second_card]
     evaluate_dealer_hand(hand)
     assert False
@@ -109,7 +204,7 @@ def test_dealer_must_hit(first_card, second_card, hit):
 
 @pytest.mark.xfail
 def test_player_wins_with_higher_score():
-    '''The player wins when their score is higher than the dealer's score.'''
+    """The player wins when their score is higher than the dealer's score."""
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.King)]
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     assert False
@@ -117,7 +212,7 @@ def test_player_wins_with_higher_score():
 
 @pytest.mark.xfail
 def test_player_loses_with_lower_score():
-    '''The player loses when their score is lower than the dealer's score.'''
+    """The player loses when their score is lower than the dealer's score."""
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.King)]
     assert False
@@ -125,7 +220,7 @@ def test_player_loses_with_lower_score():
 
 @pytest.mark.xfail
 def test_player_ties_with_same_score():
-    '''The player ties when their score is the same as the dealer's score.'''
+    """The player ties when their score is the same as the dealer's score."""
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     assert False
@@ -133,11 +228,11 @@ def test_player_ties_with_same_score():
 
 @pytest.mark.xfail
 def test_funds_increase_on_win():
-    '''The player's funds should increase when they win.
+    """The player's funds should increase when they win.
 
     The player's funds should increase by the amount they bet. In this case, the player has 100 credits
     and bets 10. The player wins and should now have 110 credits.
-    '''
+    """
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.King)]
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     player_funds = 100
@@ -147,11 +242,11 @@ def test_funds_increase_on_win():
 
 @pytest.mark.xfail
 def test_funds_decrease_on_loss():
-    '''The player's funds should decrease when they lose.
+    """The player's funds should decrease when they lose.
 
     The player's funds should decrease by the amount they bet. In this case, the player has 100 credits
     and bets 10. The player loses and should now have 90 credits.
-    '''
+    """
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.King)]
     player_funds = 100
@@ -161,11 +256,11 @@ def test_funds_decrease_on_loss():
 
 @pytest.mark.xfail
 def test_funds_stay_the_same_on_tie():
-    '''The player's funds should stay the same when they tie.
+    """The player's funds should stay the same when they tie.
 
     The player's funds should stay the same. In this case, the player has 100 credits
     and bets 10. The player ties and should still have 100 credits.
-    '''
+    """
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     [Card(suit=Suit.Spades, value=Value.Ten), Card(suit=Suit.Spades, value=Value.Eight)]
     player_funds = 100
